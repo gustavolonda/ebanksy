aca26d060fe0202bf13eccc4f7b93511b6dfa378
Gustavo Londa <gustavolonda@gmail.com>
2022-07-01 16:11:25 -0500

    [New][Create Transaction Service]

---
+++ transaction-service/src/main/java/com/iverno/gus/TransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iverno.gus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServiceApplication.class, args);
+	}
+
+}
